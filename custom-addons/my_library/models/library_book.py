6dc0d718659629ab8e81ab66f5bf9de3db60f397
trung t <tripletnov29@gmail.com>
2022-10-31 10:44:35 +0700

    add relational fields

--- custom-addons/my_library/models/library_book.py
+++ custom-addons/my_library/models/library_book.py
@@ -1,71 +1,61 @@
 # -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
-
-
-# class my_library(models.Model):
-#     _name = 'my_library.my_library'
-#     _description = 'my_library.my_library'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
 from odoo import models, fields
+
 
 class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
-    _order = 'date_release desc, name'
-    _rec_name = 'short_name'
-    
-    name = fields.Char('Title', required=True)
-    short_name = fields.Char('Short Title', required=True)
-    date_release = fields.Date('Release Date')
-    author_ids = fields.Many2many('res.partner', string='Authors')
 
+    _order = 'date_release desc, name'
+
+    name = fields.Char('Title', required=True, index=True)
+    short_name = fields.Char('Short Title',translate=True, index=True)
     notes = fields.Text('Internal Notes')
     state = fields.Selection(
-    [('draft', 'Not Available'),
-    ('available', 'Available'),
-    ('lost', 'Lost')],
-    'State')
-    description = fields.Html('Description')
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State', default="draft")
+    description = fields.Html('Description', sanitize=True, strip_style=False)
     cover = fields.Binary('Book Cover')
     out_of_print = fields.Boolean('Out of Print?')
     date_release = fields.Date('Release Date')
-    date_updated = fields.Datetime('Last Updated')
-    pages = fields.Integer('Number of Pages')
+    date_updated = fields.Datetime('Last Updated', copy=False)
+    pages = fields.Integer('Number of Pages',
+        groups='base.group_user',
+        states={'lost': [('readonly', True)]},
+        help='Total book page count', company_dependent=False)
     reader_rating = fields.Float(
-    'Reader Average Rating',
-    digits=(14, 4), # Optional precision decimals,
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision (total, decimals),
     )
+    author_ids = fields.Many2many('res.partner', string='Authors')
     cost_price = fields.Float('Book Cost', digits='Book Price')
-
     currency_id = fields.Many2one('res.currency', string='Currency')
-    retail_price = fields.Monetary('Retail Price',# optional: currency_field='currency_id',
+    retail_price = fields.Monetary('Retail Price') # optional attribute: currency_field='currency_id' incase currency field have another name then 'currency_id'
+
+    publisher_id = fields.Many2one('res.partner', string='Publisher',
+        # optional:
+        ondelete='set null',
+        context={},
+        domain=[],
     )
 
-    publisher_id = fields.Many2one('res.partner', string='Publisher', ondelete='set null', context={},domain=[], )
+    def name_get(self):
+        """ This method used to customize display name of the record """
+        result = []
+        for record in self:
+            rec_name = "%s (%s)" % (record.name, record.date_release)
+            result.append((record.id, rec_name))
+        return result
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    published_book_ids = fields.One2many(
-    'library.book', 'publisher_id',
-    string='Published Books')
 
-    authored_book_ids = fields.Many2many('library.book', string='Authored Books',)
-    # def log_all_library_members(self):
-    #     # This is an empty recordset of model library.
-    #     # member
-    #     library_member_model = self.env['library.member']
-    #     all_members = library_member_model.search([])
-    #     print("ALL MEMBERS:", all_members)
-    #     return True
+    published_book_ids = fields.One2many('library.book', 'publisher_id', string='Published Books')
+    authored_book_ids = fields.Many2many(
+        'library.book',
+        string='Authored Books',
+        # relation='library_book_res_partner_rel'  # optional
+    )
